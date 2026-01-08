4e33d33241d1b8abf8db1a5c43ab8bcda9e0ab1d
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-01-08 12:30:24 -0600

    add shove

--- main.lua
+++ main.lua
@@ -4,4 +4,8 @@ local scenery = SceneryInit(
     { path = "src.scenes.game"; key = "game"; default = "true"}
 )
 
+shove = require("src.libs.shove")
+shove.setResolution(144,128,{fitMethod="aspect",renderMode="direct"}) --pls change the resolution to what you want
+shove.setWindowMode(800, 600, {resizable = true})
+
 scenery:hook(love)
