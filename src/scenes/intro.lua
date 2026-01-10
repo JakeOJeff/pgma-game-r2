9b016fec8f1e9a18499a0b7031926e2c9a3ad658
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-01-10 09:30:49 +0530

    Adding cutscene loader

--- src/scenes/intro.lua
+++ src/scenes/intro.lua
@@ -22,41 +22,48 @@ function intro:load()
     self.fadeTimer = 0
 
     for i = 1, 3 do
-        self.scenes[i] = lg.newImage("assets/cutscenes/intro/frame"..i..".png")
+        self.scenes[i] = lg.newImage("assets/cutscenes/intro/frame" .. i .. ".png")
     end
 end
 
 function intro:update(dt)
-    player:update(dt)
+    if self.introCutscene then
+        self.cTimer = self.cTimer + dt
+        if self.fadeTimer < 1 then
+            self.fadeTimer = self.fadeTimer + (0.5 * dt)
+        end
+    else
+        player:update(dt)
 
-    cam:lookAt(player.x, player.y)
+        cam:lookAt(player.x, player.y)
 
-    if cam.x < wW / 2 then
-        cam.x = wW / 2
-    end
+        if cam.x < wW / 2 then
+            cam.x = wW / 2
+        end
 
-    if cam.y < wH / 2 then
-        cam.y = wH / 2
-    end
+        if cam.y < wH / 2 then
+            cam.y = wH / 2
+        end
 
-    local mapW = introMap.width * introMap.tilewidth
-    local mapH = introMap.height * introMap.tileheight
+        local mapW = introMap.width * introMap.tilewidth
+        local mapH = introMap.height * introMap.tileheight
 
 
-    if cam.x > (mapW - wW / 2) then
-        cam.x = (mapW - wW / 2)
-    end
-    -- Bottom border
-    if cam.y > (mapH - wH / 2) then
-        cam.y = (mapH - wH / 2)
+        if cam.x > (mapW - wW / 2) then
+            cam.x = (mapW - wW / 2)
+        end
+        -- Bottom border
+        if cam.y > (mapH - wH / 2) then
+            cam.y = (mapH - wH / 2)
+        end
     end
 end
 
 function intro:draw()
     cam:attach()
-        introMap:drawLayer(introMap.layers["Ground"])
-        introMap:drawLayer(introMap.layers["Trees"])
-        player:draw()
+    introMap:drawLayer(introMap.layers["Ground"])
+    introMap:drawLayer(introMap.layers["Trees"])
+    player:draw()
     cam:detach()
 end
 
