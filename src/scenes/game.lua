9173f4cb24cb3c8c50292f51f814ce5b085e3777
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-01-28 03:39:25 +0530

    Fix

--- src/scenes/game.lua
+++ src/scenes/game.lua
@@ -4,7 +4,7 @@ local Renderer = require("src.dungeon.renderer")
 
 local WallPhysics = require("src.dungeon.physics")
 
-local Lighter= require("src.libs.lighter-master")
+local Lighter= require("src.libs.lighter")
 function game:load()
     -- CLEAR OLD PHYSICS WORLD
     if World then
